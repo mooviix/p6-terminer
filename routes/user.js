a1fe0db9b4da517161540bc6a54835be7d734205
samy <samy.chenoufi3@gmail.com>
2022-07-06 11:37:19 +0200

    ajout regex email

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const userCtrl = require('../controllers/user');
+const checkEmail = require('../middleware/check-email');
 
 // Initialisation des routes à partir du Routeur d'Express :
-router.post('/signup', userCtrl.signup);
+router.post('/signup', checkEmail , userCtrl.signup);
 router.post('/login', userCtrl.login);
 
 module.exports = router;
