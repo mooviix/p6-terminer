7533a84808c0f0fe45948ddbba28d9d1625dbd63
samy <samy.chenoufi3@gmail.com>
2022-07-07 10:58:23 +0200

    ajout de password-validator

--- routes/user.js
+++ routes/user.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const userCtrl = require('../controllers/user');
 const checkEmail = require('../middleware/check-email');
+const checkPassword = require('../middleware/check-password');
 
 // Initialisation des routes à partir du Routeur d'Express :
-router.post('/signup', checkEmail , userCtrl.signup);
+router.post('/signup', checkEmail ,checkPassword, userCtrl.signup);
 router.post('/login', userCtrl.login);
 
 module.exports = router;
